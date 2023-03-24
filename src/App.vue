524cb8d757073c29d5cecb22b527f6326c8cddb4
Talal <talalothmanse@gmail.com>
2023-03-24 09:17:06 +0800

    -Code refactoring

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,11 @@ import { store } from './store'
 </script>
 
 <template>
-  <div class="h-screen flex flex-col justify-center items-center p-5 gap-y-5 md:gap-y-10">
+  <div class="h-screen flex flex-col justify-center items-center p-5 gap-y-5 md:gap-y-10" style="@supports (-webkit-touch-callout: none) {
+    .h-screen {
+      height: -webkit-fill-available;
+    }
+  }">
     <div class="flex flex-col justify-center items-center gap-y-2">
       <div class="flex flex-col items-center md:flex-row md:gap-x-3">
         <h1 class="text-4xl md:text-7xl font-bold">Temperature</h1>
@@ -15,8 +19,7 @@ import { store } from './store'
       <p class="text-xs md:text-lg">Convert temperature from one scale to another</p>
     </div>
     <div
-      class="border border-primary w-4/4 md:w-3/4 flex flex-col p-7 gap-y-8 rounded-lg shadow-primary shadow-md items-center"
-    >
+      class="border border-primary w-4/4 md:w-3/4 flex flex-col p-7 gap-y-8 rounded-lg shadow-primary shadow-md items-center">
       <div class="flex justify-center items-center gap-x-3">
         <Text />
         <Select :type="store.firstType" />
@@ -30,10 +33,4 @@ import { store } from './store'
   </div>
 </template>
 
-<style>
-@supports (-webkit-touch-callout: none) {
-  .h-screen {
-    height: -webkit-fill-available;
-  }
-}
-</style>
+
